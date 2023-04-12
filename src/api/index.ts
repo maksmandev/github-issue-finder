26f79ffeb4eda3c35c0af443a4235cf3d05b8676
Maksym Kulibaba <Maksym.Kulibaba@rotary.org>
2023-04-12 13:37:32 +0300

    Added token for github API (fixed request limit)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,23 +1,30 @@
-import { message } from 'antd';
-import axios from 'axios';
+import { message } from 'antd'
+import axios from 'axios'
+
+const { REACT_APP_GITHUB_ACCESS_TOKEN, REACT_APP_GITHUB_USERNAME } = process.env
+
+const token = `Basic ${btoa(
+  `${REACT_APP_GITHUB_USERNAME}:${REACT_APP_GITHUB_ACCESS_TOKEN}`
+)}`
 
 const api = axios.create({
   baseURL: 'https://api.github.com/repos/',
   headers: {
     Accept: 'application/vnd.github.v3+json',
+    Authorization: token,
   },
-});
+})
 
 api.interceptors.response.use(
   (response) => {
-    return response;
+    return response
   },
   (error) => {
     if (error.status.message === 500) {
-      message.error('Oops, something went wrong!');
+      message.error('Oops, something went wrong!')
     }
-    return Promise.reject(error.response);
+    return Promise.reject(error.response)
   }
-);
+)
 
-export default api;
+export default api
